a43d5f181dd6a9b610b3dadf0da036842d0e6d3c
Matheus <matheus.bickel@bagy.com.br>
2024-09-14 10:58:36 -0300

    task: adiciona tipagem a retornos de todos os méotodos do fluxo.

--- app/Product/Controller/ProductController.php
+++ app/Product/Controller/ProductController.php
@@ -13,30 +13,30 @@ class ProductController extends BaseController
     ) {
     }
 
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
         $prod = $this->service->getaAll();
         return response()->json($prod);
     }
 
-    public function store(Request $request)
+    public function store(Request $request): \Illuminate\Http\JsonResponse
     {
         $prod = $this->service->create($request->all());
         return response()->json($prod->toArray(), 201);
     }
 
-    public function getById(Request $request)
+    public function getById(Request $request): \Illuminate\Http\JsonResponse
     {
         return response()->json($this->service->getById($request->id));
     }
 
-    public function update(Request $request, int $id)
+    public function update(Request $request, int $id): \Illuminate\Http\JsonResponse
     {
         $res = $this->service->update($id, $request->all());
         return response()->json($res);
     }
 
-    public function destroy(int $id)
+    public function destroy(int $id): \Illuminate\Http\JsonResponse
     {
         $this->service->delete($id);
         return response()->json(null, 204);
--- app/Products/Domain/ProductService.php
+++ app/Products/Domain/ProductService.php
@@ -3,13 +3,15 @@
 namespace App\Products\Domain;
 
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 
 interface ProductService
 {
+    /**
+    * @return ProductData[]|Collection
+    */
     public function getaAll(): Collection;
-    public function getById(int $id): Model;
+    public function getById(int $id): ProductData;
     public function create(array $model): ProductData;
-    public function update(int $id, array $model): Model;
+    public function update(int $id, array $model): ProductData;
     public function delete(int $id): bool;
 }
